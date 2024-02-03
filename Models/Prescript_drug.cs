f0a74505fbb447f3fe2b19edfaf3330670bd5169
nethmi <nethmieranga20@gmail.com>
2024-02-03 21:01:45 +0530

    Prescription and Prescription drug models

---
+++ Models/Prescript_drug.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Prescript_drug
+    {
+        public string PrescriptID { get; set; }
+
+        public string GenericN { get; set; }
+
+        public float Weight { get; set; }
+
+        public String Period { get; set; }
+    }
+}
