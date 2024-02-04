0f47afa621b290a745ae69af0100a78c777ceaf1
Chathuminipamodya <ies.22.chathumini@gmail.com>
2024-02-04 20:03:30 +0530

    Drug and Bill_drug models added

---
+++ Models/Bill_drug.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    internal class Bill_drug
+    {
+        public string PrescriptID { get; set; }
+        public string DrugID { get; set; }
+        public float amount { get; set; }
+
+    }
+}
---
+++ Models/Drug.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    internal class Drug
+    {
+        public string DrugID { get; set; }
+        public string GenericN { get; set; }
+        public string BrandN { get; set; }
+        public float Weight  { get; set; }
+        public float Price { get; set; }
+        public string Avaliable { get; set; }
+    }
+}
